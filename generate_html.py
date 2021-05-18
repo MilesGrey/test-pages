1510990c1dadd3c0412e7b4c5b98500f082d2e53
Benjamin Jin <benjaminmjin@gmail.com>
2021-05-18 17:00:22 +0200

    Add index.html generation script

---
+++ generate_html.py
@@ -0,0 +1,24 @@
+from airium import Airium
+
+
+
+
+
+def generate_html():
+    a = Airium()
+
+    a('<!DOCTYPE html>')
+    with a.html():
+        with a.head():
+            a.title(_t="Speech Evaluation")
+            a.meta(name="viewport", content="width=device-width, initial-scale=1.0")
+        with a.body():
+            a.h5(_t="Hello, world!")
+
+    return str(a)
+
+
+if __name__ == '__main__':
+    html = generate_html()
+    with open('./generated_index.html', 'w') as file:
+        file.write(html)
